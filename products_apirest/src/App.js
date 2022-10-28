cb7d554d9a180aed5d60b8e2ac948e8e473ef065
AbrahamQM <abrahamqm87@gmail.com>
2022-10-28 14:55:53 +0100

    Añado rutas a la app duplicadas sirven de ejemplo para otros elementos

--- products_apirest/src/App.js
+++ products_apirest/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ListUsersComponent from './components/ListUsersComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="container">
-        <ListUsersComponent />
-      
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <HeaderComponent  />
+                    <div className="container">
+                        <Switch> 
+                            <Route path = '/' component = {ListUsersComponent}></Route>
+                            <Route path = '/users' component = {ListUsersComponent}></Route>
+                            <ListUsersComponent />
+                        </Switch>
+                    </div>
+                <FooterComponent />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
